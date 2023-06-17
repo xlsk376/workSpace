c7901513ae5c3e2c209a4bb1034a5f0fd2ae0241
xlsk376 <xlsk376@gmail.com>
2023-06-17 15:24:12 +0900

    create project

---
+++ OddEvenGanme/OddEvenGanme/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OddEvenGanme
+//
+//  Created by junyoung choi on 2023/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, junyoung!!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OddEvenGanme/OddEvenGanme/OddEvenGanmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddEvenGanmeApp.swift
+//  OddEvenGanme
+//
+//  Created by junyoung choi on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct OddEvenGanmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
